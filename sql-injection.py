22450c110cc7909a9079c36a0e93a83e376f162d
Dean Wunder <deanwunder@gmail.com>
2018-05-13 17:12:20 +1000

    Made a basic interface to take in a username and password and print out the query string being sent to the database.

---
+++ sql-injection.py
@@ -0,0 +1,12 @@
+import sqlite3
+
+connection = sqlite3.connect('sql-injection.py')
+cursor = connection.cursor()
+
+username = input('Username: ')
+password = input('Password: ')
+
+query = 'SELECT * FROM `users` WHERE `username` = %s AND `password` = %s'
+query = query % (username, password)
+print('The query being sent to the database is:')
+print(query)
