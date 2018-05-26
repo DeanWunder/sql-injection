06fb290f8764521d690558e2b0a6c235ff12f8f5
Dean Wunder <deanwunder@gmail.com>
2018-05-26 12:45:28 +1000

    Small changes

--- sql-injection.py
+++ sql-injection.py
@@ -10,3 +10,6 @@ query = 'SELECT * FROM `users` WHERE `username` = %s AND `password` = %s'
 query = query % (username, password)
 print('The query being sent to the database is:')
 print(query)
+
+cursor.execute(query)
+print('The query has successfully been executed.')
