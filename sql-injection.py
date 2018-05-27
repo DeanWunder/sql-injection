7e548fb07220c97c723512a0794add39f4774b64
Dean Wunder <deanwunder@gmail.com>
2018-05-27 21:29:17 +1000

    Made some changes, final commit.

--- sql-injection.py
+++ sql-injection.py
@@ -1,15 +1,21 @@
 import sqlite3
 
-connection = sqlite3.connect('sql-injection.py')
+connection = sqlite3.connect('sql-injection.db')
 cursor = connection.cursor()
 
 username = input('Username: ')
 password = input('Password: ')
 
-query = 'SELECT * FROM `users` WHERE `username` = %s AND `password` = %s'
+query = 'SELECT * FROM `users` WHERE `username` = "%s" AND `password` = "%s"'
 query = query % (username, password)
 print('The query being sent to the database is:')
 print(query)
 
 cursor.execute(query)
 print('The query has successfully been executed.')
+
+results = cursor.fetchall()
+if results != None and len(results) > 0:
+    print('You have been successfully authenticated.')
+else:
+    print('Access denied.')
